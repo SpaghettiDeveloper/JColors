9171908379c66bdb5f3bde3b6295ecb0709c367e
SpaghettiDeveloper <stefano.peris.dev@gmail.com>
2016-11-10 15:39:40 +0100

    add ANSI code escape + various improvements

--- src/org/lib/jcolors/Main.java
+++ src/org/lib/jcolors/Main.java
@@ -1,7 +1,9 @@
 package org.lib.jcolors;
 
+import java.io.UnsupportedEncodingException;
+
 public class Main {
-	
+	// Use the ANSI escape codes to print the colors in the console.
 	public static final String BLACK = "\u001B[30m";
 	public static final String RED = "\u001B[31m";
 	public static final String GREEN = "\u001B[32m";
@@ -14,8 +16,43 @@ public class Main {
 	// Reset code
 	public static final String RESET = "\u001B[0m";
 	
+	private static final String CHECK_MARK = "\u2713";
+	private static final String ERROR_MARK = "\u2717";
+	
+	// -----------------------------------------------------------------------------------------
+	
+	public enum PrintColor {
+
+		  BLACK("\u001B[30m"),
+		  RED("\u001B[31m"),
+		  GREEN("\u001B[32m"),
+		  YELLOW("\u001B[33m"),
+		  BLUE("\u001B[34m"),
+		  PURPLE("\u001B[35m"),
+		  CYAN("\u001B[36m"),
+		  WHITE("\u001B[37m");
+
+		  private String ansiColor;
+
+		  PrintColor(String ansiColor) {
+			  this.ansiColor = ansiColor;
+			  }
+
+		  public String getAnsiColor() {
+			  return ansiColor;
+			  }
+		  }
+	
 	public static void main(String[] args) {
 		
-	}
-
+		System.out.println(CHECK_MARK + " wrong check mark");
+	    try(PrintColorWriter out = new PrintColorWriter(System.out)) {
+	      out.println(PrintColor.GREEN, CHECK_MARK +" A green message");
+	      out.println(PrintColor.RED, ERROR_MARK +" A red message");
+	      out.green(CHECK_MARK +" A green message");
+	      out.red(ERROR_MARK +" A red message");
+	    } catch (UnsupportedEncodingException e) {
+	      e.printStackTrace();
+	      }
+	    }
 }
--- src/org/lib/jcolors/PrintColorWriter.java
+++ src/org/lib/jcolors/PrintColorWriter.java
@@ -1,25 +1,39 @@
 package org.lib.jcolors;
 
-public class PrintColorWriter extends PrintWriter {
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import org.lib.jcolors.Main.PrintColor;
+
+/*
+ * I chose to use a basic flow character instead of a byte stream as a basis Printstream.
+ * This class add a new println method that receives the color and the string to be
+ * printed on the console, also have a utility method such as green and red for the press
+ * to the desired color console.
+ */
+
+public class PrintColorWriter extends PrintWriter{
 
 	  private static final String ANSI_RESET = "\u001B[0m";
 
 	  public PrintColorWriter(PrintStream out) throws UnsupportedEncodingException {
-	    super(new OutputStreamWriter(out, "UTF-8"), true);
-	  }
+		  super(new OutputStreamWriter(out, "UTF-8"), true);
+		  }
 
 	  public void println(PrintColor color, String string) {
-	    print(color.getAnsiColor());
-	    print(string);
-	    println(ANSI_RESET);
-	    flush();
-	  }
+		  print(color.getAnsiColor());
+		  print(string);
+		  println(ANSI_RESET);
+		  flush();
+		  }
 
 	  public void green(String string) {
-	    println(PrintColor.GREEN, string);
-	  }
+		  println(PrintColor.GREEN, string);
+		  }
 
 	  public void red(String string) {
-	    println(PrintColor.RED, string);
-	  }
-	}
+		  println(PrintColor.RED, string);
+		  }
+}
